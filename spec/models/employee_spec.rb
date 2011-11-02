4d5f8a4c2e7dba06e64b26bc0b5ff44889c7fcc6
MayankJ <mayank@systematixtechnocrates.com>
2011-11-02 13:07:52 +0530

    Refine spec for data model
    This commit contains relationship check for employee_shift_assignment spec and a modified spec massage for employee .

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -10,7 +10,7 @@ describe Employee do
       @conflict = FactoryGirl.create(:conflict)
     end
 
-    it "should return all the conflict on given date" do
+    it "should return all the conflicts of employee on given date" do
       @conflict.employee.get_conflicts_on(@conflict.date).should_not be_blank
     end
   end
